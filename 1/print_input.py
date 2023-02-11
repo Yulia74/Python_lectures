eac6fd89c83959894a6d633f1e69d665cabf0cb4
[Yulia Lebedeva] <yl.75@mail.ru>
2023-02-11 11:17:52 +0500

    add print->input

---
+++ 1/print_input.py
@@ -0,0 +1,21 @@
+# Ввод и вывод данных
+# print, input
+# работа с текстовым типом данных
+
+#print('Введите а');
+#a = input()
+#print('Введите b');
+#b = input()
+#print('{} {}'.format(a,b))
+#print(f'{a} {b}')
+#print(a, ' + ', b, ' = ', a+b)
+
+# работа с целочисленным и вещественным типом данных
+
+print('Введите а')
+a = float(input())
+print('Введите b')
+b = int(input())
+print('{} {}'.format(a,b))
+print(f'{a} {b}')
+print(a, ' + ', b, ' = ', a+b)
